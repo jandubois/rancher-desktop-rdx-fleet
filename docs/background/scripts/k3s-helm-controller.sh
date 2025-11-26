45d8b8ccf3c1ede60844c1e969b5bd2f4c4655b1
Jan Dubois <jan@jandubois.com>
2025-11-25 20:25:52 -0800

    Update background for helm-controller

--- docs/background/scripts/k3s-helm-controller.sh
+++ docs/background/scripts/k3s-helm-controller.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BACKGROUND_DIR="$(dirname "$SCRIPT_DIR")"
 
 curl --silent --output "$BACKGROUND_DIR/k3s-helm-controller.md" \
-  https://raw.githubusercontent.com/k3s-io/docs/refs/heads/main/docs/helm.md
+  https://raw.githubusercontent.com/k3s-io/docs/refs/heads/main/docs/add-ons/helm.md
